5f2f56b61770b34c7e950d35ba555ef98eb9ae73
Seita Fujiwara <seitafujiwara@gmail.com>
2025-07-13 02:35:40 +0200

    feat: Added .gitignore

--- components/column_selector.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-def select_column(df):
-    numeric_columns = df.select_dtypes(include='number').columns.tolist()
-    if not numeric_columns:
-        st.warning("No numeric columns found.")
-        return None
-    return st.selectbox("Select Column", numeric_columns)
--- utils/plot.py
+++
@@ -1,5 +0,0 @@
-import plotly.express as px
-
-def plot_time_series(df, column):
-    fig = px.line(df, y=column, title=f"Time Series - {column}")
-    return fig
